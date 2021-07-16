ce61fc415824245b3e3e54f84f6beaec5d4bb44b
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-07-16 12:41:11 -0400

    remove bad env vars

--- pact/publish-pact.sh
+++ pact/publish-pact.sh
@@ -3,18 +3,14 @@
 # https://docs.pactflow.io/docs/workshops/bi-directional/contracts/oas
 
 
-
 OAS=$(cat pact/swagger-spec.yml | base64 | tr -d '\n')
 REPORT=$(echo 'tests passed' | base64)
-PACT_BROKER_BASE_URL='https://c4cneu.pactflow.io'
-PACT_BROKER_TOKEN='AiUfKlqBpw19fQGOScwsbg'
-echo $OAS
 echo "==> Uploading OAS to Pactflow"
 curl \
   -X PUT \
   -H "Authorization: Bearer ${PACT_BROKER_TOKEN}" \
   -H "Content-Type: application/json" \
-  "${PACT_BROKER_BASE_URL}/contracts/provider/jpal-backend/version/${VERSION}" \
+  "${PACT_BROKER_BASE_URL}/contracts/provider/jpal-backend/version/${GIT_COMMIT}" \
   -d "{
    \"content\": \"$OAS\",
    \"contractType\": \"oas\",
