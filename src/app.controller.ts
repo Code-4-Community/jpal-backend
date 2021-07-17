2b24acf9c2cb144a535dbe46edcc4ce192cc9ff8
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-07-17 19:16:33 -0400

    hello world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,4 +2,9 @@ import { Controller, Get, Post } from '@nestjs/common';
 import { ApiHeader } from '@nestjs/swagger';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get()
+  public hello(): string {
+    return 'Hello, World!';
+  }
+}
