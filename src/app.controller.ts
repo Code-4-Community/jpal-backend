6c39b5fbecd511518bfe603e7ede30cb7fd76d7d
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-07-17 19:05:17 -0400

    format

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,4 @@ import { Controller, Get, Post } from '@nestjs/common';
 import { ApiHeader } from '@nestjs/swagger';
 
 @Controller()
-export class AppController {
-
-}
+export class AppController {}
