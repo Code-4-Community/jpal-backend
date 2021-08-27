6fb76b99d80d9e602ab173a5ad06b973e5953bb3
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-08-26 23:40:44 -0400

    format

--- migrations/1630034606992-cognitoIsClaimed.ts
+++ migrations/1630034606992-cognitoIsClaimed.ts
@@ -1,18 +1,25 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class cognitoIsClaimed1630034606992 implements MigrationInterface {
-    name = 'cognitoIsClaimed1630034606992'
+  name = 'cognitoIsClaimed1630034606992';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "password" TO "isClaimed"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isClaimed"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "isClaimed" boolean NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" RENAME COLUMN "password" TO "isClaimed"`,
+    );
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isClaimed"`);
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "isClaimed" boolean NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isClaimed"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "isClaimed" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isClaimed" TO "password"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isClaimed"`);
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "isClaimed" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" RENAME COLUMN "isClaimed" TO "password"`,
+    );
+  }
 }
