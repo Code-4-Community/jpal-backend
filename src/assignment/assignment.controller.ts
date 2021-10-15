6e400c05a9fcedba5b8a2416defbe9e13d7488e9
Daniel Jung <danieljung335@gmail.com>
2021-10-15 16:59:00 -0400

    assignment-endpoint

--- src/assignment/assignment.controller.ts
+++ src/assignment/assignment.controller.ts
@@ -1,18 +1,23 @@
 import { Controller, Get } from "@nestjs/common";
 import { Auth } from "src/auth/decorators/auth.decorator";
-import { Role } from "src/users/types/role";
+import { ReqUser } from "src/auth/decorators/user.decorator";
+import { Role } from "src/user/types/role";
+import { User } from "src/user/types/user.entity";
+import { AssignmentService } from "./assignment.service";
+import { Assignment } from "./types/assignment.entity";
 
 @Controller('assignment')
 export class AssignmentController {
     constructor(private assignmentService: AssignmentService) {}
 
     /**
-     * Returns all surveys by the currently logged in user
+     * Returns all assignments by the currently logged in user
      */
     @Get()
-    @Auth(Role.ADMIN | Role.RESEARCHER)
-    getAllSurveys(): Promise<Assignment[]> {
-        return this.surveyService.getAllSurveys();
+    @Auth(Role.ADMIN || Role.RESEARCHER)
+    getAllAssignments(@ReqUser() user: User): Promise<Assignment[]> {
+
+        return this.assignmentService.getAllAssignments(user);
     }
     
 }
--- src/assignment/assignment.service.ts
+++ src/assignment/assignment.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from "@nestjs/common";
-import { Assignment }
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/user/types/user.entity";
+import { Repository } from "typeorm";
+import { Assignment } from "./types/assignment.entity";
+
 
 
 @Injectable()
 export class AssignmentService {
     constructor(
         @InjectRepository(Assignment) private assignmentRepository: Repository<Assignment>,
-        private awsCreateAssignment: AwsCreateUserAssignment,
     ) {}
+
+    /**
+     * gets all assignments created by the current user
+     */
+    
+    async getAllAssignments(user : User): Promise<Assignment[]> {
+        return this.assignmentRepository.find({}
+            )
+
+    }
 
 
 }
