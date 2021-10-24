2257c6ff63c215c3795b0c10e93b3cfbbb0ebf6f
onb6 <blierolivia@gmail.com>
2021-10-24 16:22:08 -0400

    prepush oopsies

--- migrations/1635106451807-add-name-to-user.ts
+++ migrations/1635106451807-add-name-to-user.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addNameToUser1635106451807 implements MigrationInterface {
-    name = 'addNameToUser1635106451807'
+  name = 'addNameToUser1635106451807';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "firstName" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "lastName" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "firstName" character varying NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "lastName" character varying NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastName"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "firstName"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastName"`);
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "firstName"`);
+  }
 }
