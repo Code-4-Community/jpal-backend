000fc75dfe1359c8962054b82bc5b0eb90d6bcf8
Vedant Rautela <v.rautela01@gmail.com>
2021-10-24 20:37:40 -0400

    implement surveyTemplate controller test

--- src/surveyTemplate/surveyTemplate.controller.spec.ts
+++ src/surveyTemplate/surveyTemplate.controller.spec.ts
@@ -1,5 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SurveyTemplateController } from './surveyTemplate.controller';
+import { SurveyTemplateService } from "./surveyTemplate.service";
+import { User } from "../user/types/user.entity";
+import { Role } from "../user/types/role";
+import { SurveyTemplate } from "./types/surveyTemplate.entity";
+
+const mockUser: User = {
+  id: 1,
+  email: 'test@test.com',
+  role: Role.ADMIN,
+};
+const mockSurveyTemplate: SurveyTemplate = { id: 1, creator: mockUser, questions: []}
+
+const serviceMock: Partial<SurveyTemplateService> = {
+  getById: jest.fn((id: number) => Promise.resolve(mockSurveyTemplate)),
+};
 
 describe('SurveyTemplateController', () => {
   let controller: SurveyTemplateController;
@@ -7,12 +22,24 @@ describe('SurveyTemplateController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SurveyTemplateController],
+      providers: [
+        {
+          provide: SurveyTemplateService,
+          useValue: serviceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<SurveyTemplateController>(SurveyTemplateController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should delegate fetching survey templates to the survey template service', async () => {
+    expect.assertions(2);
+    expect(
+      await controller.getById(1),
+    ).toEqual(mockSurveyTemplate);
+    expect(serviceMock.getById).toHaveBeenCalledWith(
+      mockSurveyTemplate.id
+    );
   });
 });
