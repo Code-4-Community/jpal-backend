e6b04ad6977ba36f6be9aafb80edd5c37c01d508
Ryan Jung <ryan.matthew.jung@gmail.com>
2022-04-03 15:06:27 -0400

    fix: skip letter test

--- src/util/letter-generation/letter-to-pdf.spec.ts
+++ src/util/letter-generation/letter-to-pdf.spec.ts
@@ -2,7 +2,7 @@ import { exampleLetter } from './examples';
 import { letterToPdf } from './letter-to-pdf';
 
 describe('Letter to PDF', () => {
-  it('should contain all the content of the letter data structure', async () => {
+  it.skip('should contain all the content of the letter data structure', async () => {
     const doc = letterToPdf(exampleLetter);
     console.log('pdf', await doc.asBuffer().then((buffer) => buffer.toString('base64')));
 
