40f46e4a95dda89285829fa29365c8ce0f169b44
Julia Finocchiaro <jgfino.dev@gmail.com>
2022-10-20 19:00:23 -0400

    format

--- migrations/1666305971601-add-start-time-to-assignment.ts
+++ migrations/1666305971601-add-start-time-to-assignment.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addStartTimeToAssignment1666305971601 implements MigrationInterface {
-    name = 'addStartTimeToAssignment1666305971601'
+  name = 'addStartTimeToAssignment1666305971601';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "assignment" ADD "started" TIMESTAMP NOT NULL DEFAULT now()`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "assignment" ADD "started" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "assignment" DROP COLUMN "started"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "assignment" DROP COLUMN "started"`);
+  }
 }
