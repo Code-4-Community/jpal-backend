94f6d6d091afd3669bff4b3d6530176f5b7aff52
huang0h <averyhuang2003@gmail.com>
2024-12-31 13:28:44 -0500

    update ccognito validator

--- src/util/cognito/cognito.wrapper.ts
+++ src/util/cognito/cognito.wrapper.ts
@@ -8,8 +8,8 @@ export class CognitoWrapper {
   private validator: Validator;
   constructor() {
     this.validator = new Validator(
-      'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_zG2SfHpXC',
-      '9v01t6v5p685510n0q5b6i9co',
+      'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_tngpF4rcu',
+      '4evkovhojuusc3nrim7ivulec7',
     );
   }
 
@@ -17,3 +17,4 @@ export class CognitoWrapper {
     return await this.validator.validate(jwt);
   }
 }
+
