4108af29448c77c2d1ea502d870c8ff0e96e2f80
Max Norman <27299284+maxn990@users.noreply.github.com>
2025-05-22 15:05:46 -0400

    Save for rebase

--- src/surveyTemplate/surveyTemplate.controller.spec.ts
+++ src/surveyTemplate/surveyTemplate.controller.spec.ts
@@ -1,29 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SurveyTemplateController } from './surveyTemplate.controller';
-import { SurveyDataQuestion, SurveyTemplateService } from './surveyTemplate.service';
+import { SurveyTemplateService } from './surveyTemplate.service';
 import { SurveyTemplate } from './types/surveyTemplate.entity';
 import { mockUser } from '../user/user.service.spec';
+import { Question } from '../question/types/question.entity';
+import { DeleteResult } from 'typeorm';
+
 
 const mockSurveyTemplate: SurveyTemplate = { id: 1, creator: mockUser, questions: [] };
 
-const serviceMock = {
-  getById: jest.fn().mockResolvedValue([
-    {
-      question: 'What is your name?',
-      options: ['Avery', 'Max'],
-    },
-  ] satisfies SurveyDataQuestion[]),
+const serviceMock: Partial<SurveyTemplateService> = {
+  getById: jest.fn(() => Promise.resolve(mockSurveyTemplate)),
+  updateSurveyTemplate: jest.fn(() => Promise.resolve(mockSurveyTemplate)),
+  deleteSurveyTemplate: jest.fn(() => Promise.resolve(mockDeleteResult))
 };
+
+const mockDeleteResult: DeleteResult = {
+  raw: [],
+  affected: 1,
+};
+
+const questions = [
+  {
+    id: 101,
+    text: 'What is your favorite color?',
+    surveyTemplate: {} as SurveyTemplate, // circular ref, safe to stub for test
+    options: [
+      {
+        id: 201,
+        text: 'Red',
+        question: {} as Question,
+      },
+      {
+        id: 202,
+        text: 'Blue',
+        question: {} as Question,
+      },
+    ],
+  },
+]
 
 describe('SurveyTemplateController', () => {
   let controller: SurveyTemplateController;
-
-  const expectedResult: SurveyDataQuestion[] = [
-    {
-      question: 'What is your name?',
-      options: ['Avery', 'Max'],
-    },
-  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -41,7 +59,17 @@ describe('SurveyTemplateController', () => {
 
   it('should delegate fetching survey templates to the survey template service', async () => {
     expect.assertions(2);
-    expect(await controller.getById(1)).toEqual(expectedResult);
+    expect(await controller.getById(1)).toEqual(mockSurveyTemplate);
     expect(serviceMock.getById).toHaveBeenCalledWith(mockSurveyTemplate.id);
+  });
+
+  it('should delegate updating survey templates to the survey template service', async () => {
+    expect.assertions(1);
+    expect(await controller.editSurveyTemplate(1, questions)).toEqual(mockSurveyTemplate)
+  });
+
+  it('should delegate deleting survey templates to the survey template service', async () => {
+    expect.assertions(1);
+    expect(await controller.deleteSurveyTemplate(1)).toEqual(mockDeleteResult);
   });
 });
--- src/surveyTemplate/surveyTemplate.service.spec.ts
+++ src/surveyTemplate/surveyTemplate.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SurveyTemplateService } from './surveyTemplate.service';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { SurveyTemplate } from './types/surveyTemplate.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { mockUser } from '../user/user.service.spec';
@@ -35,7 +35,34 @@ const mockSurveyTemplateRepository: Partial<Repository<SurveyTemplate>> = {
     if (query.where.id === 1) return mockSurveyTemplate;
     return undefined;
   },
+  save: jest.fn().mockImplementation(async (template) => template),
+  delete: jest.fn().mockImplementation(async (id: number) => mockDeleteResult)
 };
+
+const mockDeleteResult: DeleteResult = {
+  raw: [],
+  affected: 1,
+};
+
+const questions = [
+  {
+    id: 101,
+    text: 'What is your favorite color?',
+    surveyTemplate: {} as SurveyTemplate, // circular ref, safe to stub for test
+    options: [
+      {
+        id: 201,
+        text: 'Red',
+        question: {} as Question,
+      },
+      {
+        id: 202,
+        text: 'Blue',
+        question: {} as Question,
+      },
+    ],
+  },
+]
 
 describe('SurveyTemplateService', () => {
   let service: SurveyTemplateService;
@@ -68,5 +95,31 @@ describe('SurveyTemplateService', () => {
         options: ['Red', 'Blue'],
       },
     ]);
+  });
+
+  it('should return an updated survey template', async () => {
+    const surveyTemplate = await service.updateSurveyTemplate(1, questions);
+    expect(surveyTemplate).toEqual({
+      id: 1,
+      creator: mockUser,
+      questions: questions,
+    })
+  });
+
+  it('should error if the requested id is not in the table', async () => {
+    expect(async () => {
+      await service.updateSurveyTemplate(-1, questions);
+    }).rejects.toThrow();
+  });
+
+  it('should return a delete result', async () => {
+    const deleteRes = await service.deleteSurveyTemplate(1);
+    expect(deleteRes).toEqual(mockDeleteResult);
+  });
+
+  it('should error if the requested id is not in the table', async () => {
+    expect(async () => {
+      await service.updateSurveyTemplate(-1, questions);
+    }).rejects.toThrow();
   });
 });
