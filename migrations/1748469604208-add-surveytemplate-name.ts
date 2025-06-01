d4734a5c1a43fac36bc046050a7e1ec8752cc389
Avery Huang <68914997+huang0h@users.noreply.github.com>
2025-06-01 19:30:50 -0400

    get test actions working (#101)
    * correct package-lock version
    
    * update ci node version:
    
    * format + other changes
    
    * env vars to ci + comment out old tasks

--- migrations/1748469604208-add-surveytemplate-name.ts
+++ migrations/1748469604208-add-surveytemplate-name.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addSurveytemplateName1748469604208 implements MigrationInterface {
-    name = 'addSurveytemplateName1748469604208'
+  name = 'addSurveytemplateName1748469604208';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "survey_template" ADD COLUMN "name" character varying;`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "survey_template" ADD COLUMN "name" character varying;`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "survey_template" DROP COLUMN "name";`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "survey_template" DROP COLUMN "name";`);
+  }
 }
