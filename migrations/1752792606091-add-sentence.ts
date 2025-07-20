b66edcf0a4234c2ffe26e41aa76cb818f8c7142b
Max Norman <27299284+maxn990@users.noreply.github.com>
2025-07-20 16:14:24 -0400

    fix pr review comments

--- migrations/1752792606091-add-sentence.ts
+++ migrations/1752792606091-add-sentence.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class addSentence1752792606091 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "sentence" ("id" SERIAL NOT NULL, "template" character varying NOT NULL, "multiTemplate" character varying, "isPlainText" boolean NOT NULL, "isMultiQuestion" boolean NOT NULL, "includeIfSelectedOptions" text[] NOT NULL DEFAULT '{}', CONSTRAINT "PK_sentence_id" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "sentence" ("id" SERIAL NOT NULL, "template" character varying NOT NULL, "multiTemplate" character varying, "isPlainText" boolean NOT NULL, "isMultiQuestion" boolean NOT NULL, "includeIfSelectedOptions" text[] NOT NULL DEFAULT '{}', "questionId" INTEGER UNIQUE, CONSTRAINT "PK_sentence_id" PRIMARY KEY ("id"))`,
     );
 
     await queryRunner.query(`ALTER TABLE "question" ADD "sentenceId" integer UNIQUE `);
@@ -11,6 +11,11 @@ export class addSentence1752792606091 implements MigrationInterface {
     await queryRunner.query(`
       ALTER TABLE "question"
       ADD CONSTRAINT "FK_question_sentence" FOREIGN KEY ("sentenceId") REFERENCES "sentence"("id") ON DELETE CASCADE
+    `);
+
+    await queryRunner.query(`
+        ALTER TABLE "sentence"
+            ADD CONSTRAINT "FK_sentence_question" FOREIGN KEY ("questionId") REFERENCES "question"("id") ON DELETE CASCADE
     `);
   }
 
