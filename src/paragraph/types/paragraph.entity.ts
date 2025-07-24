8adc262811e8592d4694915d1bb996aa94ba6e69
lyannne <xu.ly@northeastern.edu>
2025-07-24 13:45:39 -0400

    added paragraph entity and migration

---
+++ src/paragraph/types/paragraph.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn, JoinColumn, ManyToOne, ManyToMany, JoinTable } from 'typeorm';
+import { Sentence } from 'src/sentence/types/sentence.entity';
+import {SurveyTemplate} from 'src/surveyTemplate/types/surveyTemplate.entity'
+
+@Entity()
+export class Paragraph {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  order: number;
+
+  @ManyToOne(() => SurveyTemplate)
+  template: SurveyTemplate;
+
+  @ManyToMany(() => Sentence)
+  @JoinTable()
+  sentences: Sentence[];
+}
