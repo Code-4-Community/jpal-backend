e9bd02568b0fe1b279e4d2e130a40c1817802c35
gaurirajesh <rajesh.ga@northeastern.edu>
2025-07-28 18:25:23 -0400

    get all questions route

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,6 +1,17 @@
 import { Body, Controller, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
-import { QuestionService } from './question.service';
+import { QuestionData, QuestionService } from './question.service';
+import {Auth} from '../auth/decorators/auth.decorator'
+import { Role } from '../user/types/role';
 @Controller('question')
 export class QuestionController {
   constructor(private questionService: QuestionService) {}
+
+  /**
+   * Gets all the questions.
+   */
+  @Get('/questions')
+  @Auth(Role.ADMIN, Role.RESEARCHER)
+  async getAllQuestions(): Promise<QuestionData[]> {
+    return await this.questionService.getAllQuestions();
+  }
 }
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -5,6 +5,24 @@ import { Question } from './types/question.entity';
 import { Sentence } from '../sentence/types/sentence.entity';
 import { Option } from '../option/types/option.entity';
 
+export interface QuestionData {
+  id: number;
+  text: string;
+  template: string;
+  options: Option[];
+}
+
+export function transformToQuestionData(
+  questionEntities: Question[],
+): QuestionData[] {
+  return questionEntities.map((q) => ({
+    id: q.id,
+    text: q.text,
+    template: q.sentence.template,
+    options: q.options
+  }));
+}
+
 @Injectable()
 export class QuestionService {
   private logger = new Logger(QuestionService.name);
@@ -14,4 +32,9 @@ export class QuestionService {
     @InjectRepository(Sentence) private sentenceRepository: Repository<Sentence>,
     @InjectRepository(Sentence) private optionRepository: Repository<Option>,
   ) {}
+
+  async getAllQuestions(): Promise<QuestionData[]> {
+    const result = await this.questionRepository.find({});
+    return transformToQuestionData(result);
+  }
 }
