a901f98a19ea83e224f0246ef6e44cb2bc87c09f
lyannne <xu.ly@northeastern.edu>
2025-07-31 02:37:11 -0400

    controller test i think?

--- src/question/question.controller.spec.ts
+++ src/question/question.controller.spec.ts
@@ -3,49 +3,81 @@ import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
 import { Sentence } from './../sentence/types/sentence.entity';
 import { Question } from './types/question.entity';
-import { exampleOptions } from './question.examples';
-import { UploadQuestionsDTO } from './dto/upload-question.dto';
+import { exampleOptions, FIVE_SENTIMENT_OPTIONS, NEVER, RARELY, OFTEN, ALWAYS } from './question.examples';
+import { UploadQuestionsDTO, UploadQuestionResponseDTO } from './dto/upload-question.dto';
 
-
-
-const plaintextsentences: string[] = [
-  "Youth worked at Accelerate Academy",
-  "Youth worked July - August of 2023"
-]
-
-const question1 = {
+class questionDTO {
   text: string;
   options: string[];
   sentence_template: string;
   include_if_selected_options: string[];
 }
 
-const questionInfo: string[] = [
+const question1: questionDTO = {
+  text: "responsible",
+  options: FIVE_SENTIMENT_OPTIONS,
+  sentence_template: "Youth was _____",
+  include_if_selected_options: [OFTEN, ALWAYS],
+};
+
+const question2: questionDTO = {
+  text: "timely",
+  options: FIVE_SENTIMENT_OPTIONS,
+  sentence_template: "Youth was ____",
+  include_if_selected_options: [NEVER, RARELY],
+};
+
+const questionInfo: questionDTO[] = [
+  question1, question2
+]
+
+class multiQuestionDTO {
+  sentence_template: string;
+  fragment_texts: string[];
+  question_texts: string[];
+  options: string[][];
+  include_if_selected_option: string[];
+}
+
+const multiquestion1: multiQuestionDTO = {
+  sentence_template: "Youth was a ___, ___, and ___ employee",
+  fragment_texts: ["responsible", "timely", "hard-working"],
+  question_texts: ["was youth a responsible employee?", "was youth a timely employee?", "was youth a hard-working employee?"],
+  options: [
+    ["yes", "no"],
+    ["yes", "no"],
+    ["yes", "no"]
+  ],
+  include_if_selected_option: ["yes", "yes", "yes"]
+};
+
+const multiQuestionInfo: multiQuestionDTO[] = [
+  multiquestion1
+]
+
+const plaintextsentences: string[] = [
   "Youth worked at Accelerate Academy",
   "Youth worked July - August of 2023"
 ]
+
+
 export const mockUploadQuestionDTO: UploadQuestionsDTO = {
-  questions: ,
-  multi_questions: ,
+  questions: questionInfo,
+  multi_questions: multiQuestionInfo,
   plain_text: plaintextsentences,
 };
 
-export const mockQuestion1: Question = {
-  id: 1,
-  text: 'How often is this student responsible?',
-  options: exampleOptions,
-  sentence: new Sentence(),
+export const mockUploadQuestionResponseDTO: UploadQuestionResponseDTO = {
+  questions: 2,
+  multi_question_sentences: 3,
+  plain_text_sentences: 2
 };
 
-export const mockQuestion2: Question = {
-  id: 2,
-  text: 'How often did this student arrive on time for work?',
-  options: exampleOptions,
-  sentence: new Sentence(),
-};
 
 export const mockQuestionService: Partial<QuestionService> = {
-  getAllQuestions: jest.fn(() => Promise.resolve([mockReturnedQuestion1, mockReturnedQuestion2])),
+  batchCreatePlainText: jest.fn(() => Promise.resolve(2)),
+  batchCreateQuestions: jest.fn(() => Promise.resolve(2)),
+  batchCreateMultiQuestions: jest.fn(() => Promise.resolve(3)),
 };
 
 describe('QuestionController', () => {
@@ -69,13 +101,14 @@ describe('QuestionController', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('getSurveyAssignments', () => {
-    it('should return questions', async () => {
-      expect(await controller.getAllQuestions()).toEqual([
-        mockReturnedQuestion1,
-        mockReturnedQuestion2,
+  describe('create questions', () => {
+    it('should return number of sentences and questions made', async () => {
+      expect(await controller.create(mockUploadQuestionDTO)).toEqual([
+        mockUploadQuestionResponseDTO
       ]);
-      expect(mockQuestionService.getAllQuestions).toHaveBeenCalled();
+      expect(mockQuestionService.batchCreateMultiQuestions).toHaveBeenCalled();
+      expect(mockQuestionService.batchCreatePlainText).toHaveBeenCalled();
+      expect(mockQuestionService.batchCreateQuestions).toHaveBeenCalled();
     });
   });
 });
