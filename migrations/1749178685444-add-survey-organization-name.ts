f4147f4c0833ea19d8bbd9fb25a12366395beb01
Max Norman <27299284+maxn990@users.noreply.github.com>
2025-08-09 18:23:43 -0400

    formatting

--- migrations/1749178685444-add-survey-organization-name.ts
+++ migrations/1749178685444-add-survey-organization-name.ts
@@ -3,7 +3,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class addSurveyOrganizationName1749178685444 implements MigrationInterface {
   name = 'addSurveyOrganizationName1749178685444';
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "survey" ADD "organizationName" character varying not null`);
+    await queryRunner.query(
+      `ALTER TABLE "survey" ADD "organizationName" character varying not null`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
